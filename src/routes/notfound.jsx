4712381b8d6def2de7c662660cfb90a5572dcc7b
Jace <jacealan1@gmail.com>
2023-02-11 21:22:01 +0900

    change 404

--- src/routes/notfound.jsx
+++ src/routes/notfound.jsx
@@ -21,16 +21,10 @@ function NotFound() {
         <Box
           w={{ base: "400px", md: "700px" }}
           h={{ base: "400px", md: "700px" }}
-          bgImage={"/404.jpg"}
+          bgImage={"/404.png"}
           bgSize={"cover"}
           position={"relative"}
-        >
-          <Box position={"absolute"} bottom={0} w={"100%"}>
-            <Center w={"100%"} color={"#DDD"}>
-              designed by <a href="//vecteezy.com">vecteezy.com</a>
-            </Center>
-          </Box>
-        </Box>
+        />
       </Center>
     </>
   )
